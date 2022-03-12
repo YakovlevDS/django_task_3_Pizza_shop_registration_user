11c24e77d376196583024208d1996dd53c61624f
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-12 07:40:30 +0200

    feat:add low level auth

--- authapp/forms.py
+++ authapp/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from authapp.models import PizzaShop
 
 class UserForm(forms.ModelForm):
+       email = forms.CharField(max_length=100, required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ('username', 'password', 'first_name', 'last_name', 'email')
